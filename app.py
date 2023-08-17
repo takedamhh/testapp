1d5814c3583e8e03d9f3685b295af344989bcdfe
takedamhh <takedam.hogehoge@gmail.com>
2023-08-17 01:35:49 +0000

    update

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    # Output request headers "TAKEDA" to Log
-    app.logger.info(request.headers.get("TAKEDA"))
+    # # Output request headers "TAKEDA" to Log
+    # app.logger.info(request.headers.get("TAKEDA"))
 
-    # Return current time
-    dt_now = datetime.datetime.now()
-    return dt_now.strftime('%Y-%m-%d %H:%M:%S') 
+    # # Return current time
+    # dt_now = datetime.datetime.now()
+    # return dt_now.strftime('%Y-%m-%d %H:%M:%S')
 
+    return 'Hello World'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
